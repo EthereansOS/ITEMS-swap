49cdc6ef91a09f4d07270068c91ceb38103e7d4f
Marco Vasapollo <ceo@metaring.com>
2021-03-31 01:14:02 +0200

    api path

--- src/constants/ethItemLists/src/index.js
+++ src/constants/ethItemLists/src/index.js
@@ -3,7 +3,7 @@ require('./utils')
 const fs = require('fs')
 const path = require('path')
 const createClient = require('ipfs-http-client')
-const ipfs = createClient('ipfs.infura.io', '5001', { protocol: 'https' })
+const ipfs = createClient({host: 'ipfs.infura.io', port: '5001', protocol: 'https' , apiPath: '/ipfs/api/v0'});
 
 window.context.blockchainConnectionString =
   window.context.blockchainConnectionString || process.env.BLOCKCHAIN_CONNECTION_STRING
